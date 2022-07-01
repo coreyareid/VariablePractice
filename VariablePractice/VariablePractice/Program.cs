66127bc9965bbb46998a5208c9aa54a3557888bb
coreyareid <coreyareid@bellsouth.net>
2022-07-01 11:31:36 -0500

    Changed practice

--- VariablePractice/VariablePractice/Program.cs
+++ VariablePractice/VariablePractice/Program.cs
@@ -12,18 +12,33 @@ namespace VariablePractice
             Console.WriteLine("Now, what is your name?");
                 string myName = Console.ReadLine();
 
-            Console.WriteLine($" Your name is {myName}, and you are {myAge} years old");
-
+            bool ofAge = true;
             if (myAge > 18)
             {
-                Console.WriteLine("You are of age to use this website");
-            } else if (myAge < 18)
-            {
-                Console.WriteLine("Sorry, you cannot use this system.");
+                ofAge = true;
             } else
             {
-                Console.WriteLine("You did not enter your age.");
+                ofAge = false;
             }
+
+            char letter = 'a';
+
+            double number1 = 20;
+
+            decimal number2 = 5.5M;
+
+            Console.WriteLine($" Your name is {myName}, and you are {myAge} years old");
+            if (ofAge)
+            {
+                Console.WriteLine($" Are you of age? {ofAge}");
+            } else
+            {
+                Console.WriteLine($" Are you of age? {ofAge}");
+            }
+
+            Console.WriteLine($" you\'re in line {letter} and in position {number1}");
+            Console.WriteLine($"Your time in line is {number2}");
+            
         }
     }
 }
