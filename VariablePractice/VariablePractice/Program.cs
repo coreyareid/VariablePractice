145aa4fd45ca8c2fbe0328895944c138b9b4273a
coreyareid <coreyareid@bellsouth.net>
2022-07-01 11:42:18 -0500

    changed around some lines to make sense

--- VariablePractice/VariablePractice/Program.cs
+++ VariablePractice/VariablePractice/Program.cs
@@ -6,13 +6,15 @@ namespace VariablePractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your age?");
-            int myAge = Convert.ToInt32(Console.ReadLine());
+   
 
             Console.WriteLine("Now, what is your name?");
                 string myName = Console.ReadLine();
 
+            Console.WriteLine("What is your age?");
+            int myAge = Convert.ToInt32(Console.ReadLine());
             bool ofAge = true;
+
             if (myAge > 18)
             {
                 ofAge = true;
@@ -27,17 +29,19 @@ namespace VariablePractice
 
             decimal number2 = 5.5M;
 
-            Console.WriteLine($" Your name is {myName}, and you are {myAge} years old");
+            Console.WriteLine($"Your name is {myName}, and you are {myAge} years old");
             if (ofAge)
             {
-                Console.WriteLine($" Are you of age? {ofAge}");
+                Console.WriteLine($"Are you of age? {ofAge}");
+                Console.WriteLine($"You\'re in line {letter} and in position{number1}.");
+                Console.WriteLine($"Your time in line is {number2} minutes.");
             } else
             {
-                Console.WriteLine($" Are you of age? {ofAge}");
+                Console.WriteLine($"Are you of age? {ofAge}.");
+                Console.WriteLine("You are not of age, goodbye.");
             }
 
-            Console.WriteLine($" you\'re in line {letter} and in position {number1}");
-            Console.WriteLine($"Your time in line is {number2}");
+            
             
         }
     }
