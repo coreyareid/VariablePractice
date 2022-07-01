b4dd2edd0afa4f700b7f4d5d0830b0b5255097a2
coreyareid <coreyareid@bellsouth.net>
2022-07-01 10:21:20 -0500

    added if-else statements

--- VariablePractice/VariablePractice/Program.cs
+++ VariablePractice/VariablePractice/Program.cs
@@ -6,10 +6,24 @@ namespace VariablePractice
     {
         static void Main(string[] args)
         {
-            string dogName = "Meeka";
-            int dogAge = 3;
+            Console.WriteLine("What is your age?");
+            int myAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"My dog\'s name is {dogName}, She is {dogAge} years old.");
+            Console.WriteLine("Now, what is your name?");
+                string myName = Console.ReadLine();
+
+            Console.WriteLine($" Your name is {myName}, and you are {myAge} years old");
+
+            if (myAge > 18)
+            {
+                Console.WriteLine("You are of age to use this website");
+            } else if (myAge < 18)
+            {
+                Console.WriteLine("Sorry, you cannot use this system.");
+            } else
+            {
+                Console.WriteLine("You did not enter your age.");
+            }
         }
     }
 }
